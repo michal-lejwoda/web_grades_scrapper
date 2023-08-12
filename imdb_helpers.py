d8c8211f1a85ea875fb5e1370fad5cb523a2a2e7
michal <m.lejwoda@sunshineprofits.com>
2023-08-12 11:48:23 +0200

    create imdb types

--- imdb_helpers.py
+++ imdb_helpers.py
@@ -1,4 +1,5 @@
 import bs4
+import imdb_types
 def get_imdb_movies_list(soup: bs4.BeautifulSoup) -> list:
     res_arr = []
     all_results = soup.select('.ipc-metadata-list-summary-item')
@@ -14,55 +15,55 @@ def get_imdb_movies_list(soup: bs4.BeautifulSoup) -> list:
 
 def detail_imdb_movie(soup: bs4.BeautifulSoup) -> dict:
     container = soup.find(name='main')
-    title = container.find(name="span", attrs={"class": "sc-afe43def-1"}).text
-    data_container = container.find(name="ul", attrs={"class": "sc-afe43def-4"})
+    title = container.find(name="span", attrs={"class": imdb_types.TITLE}).text
+    data_container = container.find(name="ul", attrs={"class": imdb_types.DATA_CONTAINER})
     data_results = data_container.find_all(name="a")
     data = []
     for data_res in data_results:
         data.append(data_res.text)
     imdb_rating_container = container.find(name="div",
-                                           attrs={"data-testid": "hero-rating-bar__aggregate-rating__score"}).find_all(
+                                           attrs={"data-testid": imdb_types.IMDB_RATING_CONTAINER}).find_all(
         name="span")
     imdb_rating = "{}{}".format(imdb_rating_container[0].text, imdb_rating_container[1].text)
-    imdb_rating_based_on = container.find(name="div", attrs={"class": "bjjENQ"}).text
+    imdb_rating_based_on = container.find(name="div", attrs={"class": imdb_types.IMDB_RATING_BASED_ON}).text
     # Nie zawsze jest
-    popularity = container.find(name="div", attrs={"data-testid": "hero-rating-bar__popularity__score"}).text
-    presentations_container = container.find(name="div", attrs={"class": "hKIseD"}).find_all(name="li", attrs={
+    popularity = container.find(name="div", attrs={"data-testid": imdb_types.POPULARITY}).text
+    presentations_container = container.find(name="div", attrs={"class": imdb_types.PRESENTATION_CONTAINER}).find_all(name="li", attrs={
         "class": "ipc-metadata-list__item"})
     presentations = []
     for presentation in presentations_container:
         try:
-            label = presentation.find(name="span", attrs={"class": "ipc-metadata-list-item__label"}).text
+            label = presentation.find(name="span", attrs={"class": imdb_types.PRESENTATION_ELEMENTS_LABEL}).text
         except:
-            label = presentation.find(name="a", attrs={"class": "ipc-metadata-list-item__label"}).text
-        li_presentation = presentation.find_all(name="li", attrs={"class": "ipc-inline-list__item"})
+            label = presentation.find(name="a", attrs={"class": imdb_types.PRESENTATION_ELEMENTS_LABEL}).text
+        li_presentation = presentation.find_all(name="li", attrs={"class": imdb_types.NESTED_PRESENTATION_ELEMENTS})
         li_presentation_results = []
         for li in li_presentation:
             li_presentation_results.append(li.find(name="a").text.strip())
         presentations.append({"label": label, "presentation": li_presentation_results})
-    metascore = container.find(name="span", attrs={"class": "score-meta"}).text
-    user_reviews_number = container.find_all(name="span", attrs={"class": "score"})[0].text
-    critic_reviews_number = container.find_all(name="span", attrs={"class": "score"})[1].text
-    photos_container = container.find_all(name="div", attrs={"class": "ipc-media ipc-media--photo"})
+    metascore = container.find(name="span", attrs={"class": imdb_types.METASCORE}).text
+    user_reviews_number = container.find_all(name="span", attrs={"class": imdb_types.SCORE})[0].text
+    critic_reviews_number = container.find_all(name="span", attrs={"class": imdb_types.SCORE})[1].text
+    photos_container = container.find_all(name="div", attrs={"class": imdb_types.PHOTOS_CONTAINER})
     photos = []
     for photo in photos_container:
         temp_photo = {"src": photo.find(name="img")["src"], "alt": photo.find(name="img")["alt"]}
         photos.append(temp_photo)
-    actors_container = container.find_all(name="div", attrs={"class": "kUzsHJ"})
+    actors_container = container.find_all(name="div", attrs={"class": imdb_types.ACTORS_CONTAINER})
     actors = []
     for actor in actors_container:
         if actor.find(name="img") == None:
             actor_img = {"src": None, "alt": None}
         else:
             actor_img = {"src": actor.find(name="img")['src'], "alt": actor.find(name="img")['alt']}
-        actor_name = actor.find(name="a", attrs={"class": "fUguci"}).text
-        actor_character = actor.find(name="span", attrs={"class": "llsTve"}).text
+        actor_name = actor.find(name="a", attrs={"class": imdb_types.ACTOR_NAME}).text
+        actor_character = actor.find(name="span", attrs={"class": imdb_types.ACTOR_CHARACTER}).text
         actors.append({"actor_img": actor_img, "actor_name": actor_name, "actor_character": actor_character})
-    more_like_this_container = container.find_all(name="div", attrs={"class": "ipc-poster-card"})
+    more_like_this_container = container.find_all(name="div", attrs={"class": imdb_types.MORE_LIKE_THIS_CONTAINER})
     more_like_this_arr = []
     for more_like_this in more_like_this_container:
-        more_like_this_title = more_like_this.find(name="span", attrs={"data-testid": "title"}).text
-        more_like_this_rating = more_like_this.find(name="div", attrs={"class": "ipc-rating-star-group"}).find(
+        more_like_this_title = more_like_this.find(name="span", attrs={"data-testid": imdb_types.MORE_LIKE_THIS_ELEMENT_TITLE}).text
+        more_like_this_rating = more_like_this.find(name="div", attrs={"class": imdb_types.MORE_LIKE_THIS_ELEMENT_RATING}).find(
             name="span").text
         more_like_this_image = {"src": more_like_this.find(name="img")["src"],
                                 "alt": more_like_this.find(name="img")["alt"]}
