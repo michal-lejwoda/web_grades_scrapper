a8cd285d22be3adebb05026f4303254b22253146
michal <m.lejwoda@sunshineprofits.com>
2023-08-19 21:23:06 +0200

    change get requests to post

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ async def say_hello(name: str):
 @app.post("/metacritic")
 async def metacritic(name_schema: NameSchema) -> list:
     name = name_schema.name
-    # name = "red dead redemption 2"
     url_template = "https://www.metacritic.com/search/game/{}/results"
     url = create_url(url_template, name)
     soup = get_soup(url)
@@ -41,7 +40,6 @@ async def metacritic(name_schema: NameSchema) -> list:
 async def opencritic(name_schema: NameSchema) -> Optional[list]:
     #TODO: GET More Data and add url
     name = name_schema.name
-    # content = "red dead redemption 2"
     url_template = "https://opencritic-api.p.rapidapi.com/game/search"
     opencritic_games_list = get_opencritic_games_list_json(url_template, name)
     if opencritic_games_list == None:
@@ -52,7 +50,6 @@ async def opencritic(name_schema: NameSchema) -> Optional[list]:
 @app.post("/imdb")
 async def imdb(name_schema: NameSchema) -> list:
     name = name_schema.name
-    # name = "red dead redemption 2"
     name_slugify = slugify(name)
     url_template = "https://www.imdb.com/find/?q={}&ref_=nv_sr_sm"
     url = create_url(url_template, name_slugify)
@@ -60,25 +57,28 @@ async def imdb(name_schema: NameSchema) -> list:
     return get_imdb_movies_list(soup)
 
 
-@app.get("/metacritic_detail")
+@app.post("/metacritic_detail")
 async def metacritic_detail(urlschema: UrlSchema):
-    url_template = "https://www.metacritic.com/game/xbox-one/red-dead-redemption-2"
+    url_template = urlschema.url
+    # url_template = "https://www.metacritic.com/game/xbox-one/red-dead-redemption-2"
     soup = get_soup(url_template)
     game_detail = detail_metacritic_games(soup)
     return game_detail
 
 
-@app.get("/opencritic_detail")
+@app.post("/opencritic_detail")
 async def opencritic_detail(urlschema: UrlSchema):
+    url_template = urlschema.url
     url_template = "https://opencritic.com/game/10993/deathloop"
     soup = get_soup(url_template)
     game_detail = detail_opencritic_games(soup)
     return game_detail
 
 
-@app.get("/imdb_detail")
+@app.post("/imdb_detail")
 async def imdb_detail(urlschema: UrlSchema):
-    url_template = "https://www.imdb.com/title/tt0120815/"
+    url_template = urlschema.url
+    # url_template = "https://www.imdb.com/title/tt0120815/"
     soup = get_soup(url_template)
     movie_detail = detail_imdb_movie(soup)
     return movie_detail
