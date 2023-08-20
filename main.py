970063a06753765cdb8c172de8f722dfc01acdb3
michal <m.lejwoda@sunshineprofits.com>
2023-08-20 13:58:29 +0200

    some changes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from typing import Optional
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from slugify import slugify
-from dotenv import load_dotenv
 
 from helpers import get_soup, create_url
 from imdb.imdb_helpers import get_imdb_movies_list, detail_imdb_movie
 from metacritic.metacritic_helpers import list_metacritic_games, detail_metacritic_games
 from opencritic.opencritic_helpers import get_opencritic_games_list_json, detail_opencritic_games
-from schemas import NameSchema, UrlSchema
+from schemas import NameSchema, UrlOpencriticSchema, UrlMetacriticSchema, UrlImdbSchema
 
 load_dotenv('.env')
 app = FastAPI()
@@ -23,62 +22,59 @@ async def root():
 
 
 @app.post("/metacritic")
-async def metacritic(name_schema: NameSchema) -> list:
+async def metacritic(name_schema: NameSchema):
     name = name_schema.name
     url_template = "https://www.metacritic.com/search/game/{}/results"
     url = create_url(url_template, name)
     soup = get_soup(url)
     list_elements = list_metacritic_games(soup)
-    if list_elements == None:
+    if list_elements is None:
         return "Can't get data. :("
     return list_elements
 
 
 @app.post("/opencritic")
-async def opencritic(name_schema: NameSchema) -> Optional[list]:
+async def opencritic(name_schema: NameSchema):
     name = name_schema.name
     url_template = "https://opencritic-api.p.rapidapi.com/game/search"
     opencritic_games_list = get_opencritic_games_list_json(url_template, name)
-    if opencritic_games_list == None:
+    if opencritic_games_list is None:
         return "Can't get data. :("
     return opencritic_games_list
 
 
 @app.post("/imdb")
-async def imdb(name_schema: NameSchema) -> list:
+async def imdb(name_schema: NameSchema):
     name = name_schema.name
     name_slugify = slugify(name)
     url_template = "https://www.imdb.com/find/?q={}&ref_=nv_sr_sm"
     url = create_url(url_template, name_slugify)
     soup = get_soup(url)
     list_elements = get_imdb_movies_list(soup)
-    if list_elements == None:
+    if list_elements is None:
         return "Can't get data. :("
     return list_elements
 
 
 @app.post("/metacritic_detail")
-async def metacritic_detail(urlschema: UrlSchema):
+async def metacritic_detail(urlschema: UrlMetacriticSchema):
     url_template = urlschema.url
-    # url_template = "https://www.metacritic.com/game/xbox-one/red-dead-redemption-2"
     soup = get_soup(url_template)
     game_detail = detail_metacritic_games(soup)
     return game_detail
 
 
 @app.post("/opencritic_detail")
-async def opencritic_detail(urlschema: UrlSchema):
+async def opencritic_detail(urlschema: UrlOpencriticSchema):
     url_template = urlschema.url
-    # url_template = "https://opencritic.com/game/10993/deathloop"
     soup = get_soup(url_template)
     game_detail = detail_opencritic_games(soup)
     return game_detail
 
 
 @app.post("/imdb_detail")
-async def imdb_detail(urlschema: UrlSchema):
+async def imdb_detail(urlschema: UrlImdbSchema):
     url_template = urlschema.url
-    # url_template = "https://www.imdb.com/title/tt0120815/"
     soup = get_soup(url_template)
     movie_detail = detail_imdb_movie(soup)
     return movie_detail
--- schemas.py
+++ schemas.py
@@ -5,5 +5,13 @@ class NameSchema(BaseModel):
     name: str
 
 
-class UrlSchema(BaseModel):
-    url: str
+class UrlOpencriticSchema(BaseModel):
+    url: str = "https://opencritic.com/game/10993/deathloop"
+
+
+class UrlMetacriticSchema(BaseModel):
+    url: str = "https://www.metacritic.com/game/xbox-one/red-dead-redemption-2"
+
+
+class UrlImdbSchema(BaseModel):
+    url: str = "https://www.imdb.com/title/tt0120815/"
