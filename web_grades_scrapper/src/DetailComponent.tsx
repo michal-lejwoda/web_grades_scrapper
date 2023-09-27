2dfa717c9d513ee0c2418d93af5451ebbcf42dee
michal <m.lejwoda@sunshineprofits.com>
2023-09-27 10:57:14 +0200

    Navigation in DetailComponent.tsx

--- web_grades_scrapper/src/DetailComponent.tsx
+++ web_grades_scrapper/src/DetailComponent.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import {useLocation} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
 import MetacriticDetails from "./details/MetacriticDetails.tsx";
 import OpencriticDetails from "./details/OpencriticDetails.tsx";
 import ImdbDetails from "./details/ImdbDetails.tsx";
 
 function DetailComponent() {
     const location = useLocation()
-    console.log("location")
-    console.log(location)
-    if(location.state.page == "metacritic")
-        return (<MetacriticDetails url={location.state.url} />)
-    if(location.state.page == "opencritic")
-        return (<OpencriticDetails url={location.state.url} />)
-    if(location.state.page == "imdb")
-        return (<ImdbDetails url={location.state.url} />)
+    const navigate = useNavigate()
+    const handleNavigationtoList = () => {
+        navigate("/")
+    }
+    useEffect(() => {
+        if(location.state == null)
+            return handleNavigationtoList()
+    }, []);
+
+    if (location.state !== null) {
+        if (location.state.page == "metacritic")
+            return (<MetacriticDetails url={location.state.url}/>)
+        if (location.state.page == "opencritic")
+            return (<OpencriticDetails url={location.state.url}/>)
+        if (location.state.page == "imdb")
+            return (<ImdbDetails url={location.state.url}/>)
+    }
+
 
 }
 
