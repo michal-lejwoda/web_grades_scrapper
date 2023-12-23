9a938c942563a79759779383011e769670ae0468
michal <m.lejwoda@sunshineprofits.com>
2023-12-23 16:42:35 +0100

    list opencritic changes

--- web_grades_scrapper/src/lists/OpencriticList.tsx
+++ web_grades_scrapper/src/lists/OpencriticList.tsx
@@ -3,11 +3,31 @@ import {PropsList} from "../interfaces.tsx";
 
 
 function OpencriticList(props: PropsList) {
-    const list_of_opencritic_elements = props.data.map((element) => <div
-        onClick={() => props.handleNavigation(element.url, "opencritic")} key={element.id}>{element.name}</div>)
+    const list_of_opencritic_elements = props.data.map((element) =>
+        <li className="py-3 sm:pb-4 cursor-pointer" onClick={() => props.handleNavigation(element.url, "opencritic")}>
+                    <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                        <div className="flex-shrink-0">
+                            {element.img ? <img className="w-14 h-14 rounded-full" src={element.img.src} alt=""/>: ""}
+                        </div>
+                        <div className="flex-1 min-w-0">
+                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                {element.name}
+                            </p>
+                        </div>
+                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                            {element.critic_score}
+                        </div>
+                    </div>
+                </li>
+
+    )
     return (
         <>
-            {list_of_opencritic_elements}
+            <ul
+                className="w-4/5 lg:w-2/5 divide-y divide-gray-200 dark:divide-gray-700"
+            >
+                {list_of_opencritic_elements}
+            </ul>
         </>
     )
 }
